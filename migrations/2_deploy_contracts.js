24c00915aa393cfcef82f45effa726ec5b9fea56
Nemil Dalal <nemild@gmail.com>
2018-10-06 12:40:24 -0700

    Test scaffold

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,8 @@
-const TweEthVoter = artifacts.require('TweEthVoter');
 const ERC20Mintable = artifacts.require('ERC20Mintable');
+const TweEthVoter = artifacts.require('TweEthVoter');
 
 module.exports = async function(deployer, network, accounts) {
-  await deployer.deploy(ERC20Mintable, {from: accounts[0]});
+  await deployer.deploy(ERC20Mintable);
+  console.log(ERC20Mintable.address);
   await deployer.deploy(TweEthVoter, ERC20Mintable.address, 5, 0, {from: accounts[0]});
 }
--- test/TweEth.js
+++ test/TweEth.js
@@ -1,12 +1,19 @@
-const TweEth = artifacts.require('TweEth');
-const TweEth = artifacts.require('TweEth');
+const ERC20Mintable = artifacts.require('ERC20Mintable');
+const TweEthVoter = artifacts.require('TweEthVoter');
 
-contract('TweEth', async (accounts) => {
-  it("should allow the creation of a propsal", async () => {
-    let instance = await TweEth.deployed();
+contract('TweEthVoter', async (accounts) => {
+  it('should allow the creation of a proposal', async () => {
+    let erc20Mintable = await ERC20Mintable.new({from: accounts[0]});
+    const tweEthVoter = await TweEthVoter.new(erc20Mintable.address, 5, 0, {from: accounts[0]});
 
-    let instance = await TweEth.deployed();
-    let balance = await instance.getBalance.call(accounts[0]);
-    assert.equal(balance.valueOf(), 10000);
+    console.log(erc20Mintable.address);
+
+    await erc20Mintable.mint.call(accounts[0], 10, {from: accounts[0]});
+    const result = await erc20Mintable.totalSupply.call();
+    console.log(result.toString());
+
+    // let instance = await TweEthVoter.deployed(erc20Mintable.address, 5, 0);
+
+    // assert.equal(balance.valueOf(), 10000);
   });
 });
