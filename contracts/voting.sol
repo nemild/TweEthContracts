e355134fe1ce0d914b959e1f297ae421c119aef8
Nemil Dalal <nemild@gmail.com>
2018-10-06 01:51:06 -0700

    Initial version of voting

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -1,15 +1,17 @@
-pragma solidity ^0.4.19;
-/// @title TwitterVoter
+pragma solidity ^0.4.25;
+
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol";
+
+/// @title TweEthVoter
 /// @author nemild mtlapinski
 
-contract TwitterVoter { // CapWords
-    using SafeMath for uint256;
+contract TweEthVoter { // CapWords
+  using SafeMath for uint256;
 
-
-  address private tokenAddress; 
+  ERC20Mintable private tokenAddress; 
   uint256 private votingLength = 10 minutes;
   uint256 private quorumTokensPercentage;
-  uint8 private winPercent = 50;
   uint256 private proposerAmount; // the amount the proposer has staked to submit the tweet 
 
   struct Proposal {
@@ -22,15 +24,16 @@ contract TwitterVoter { // CapWords
     uint256 noTotal;
     uint256 yesTotal;
     uint256 bonus;
+
     bool yesWon;
+    bool quorumPassed;
   };
 
-  mapping(uint256 => Proposal) private uuidToProposals;
-  mapping (address => uint256) private balances;
-
+  mapping(bytes32 => Proposal) private uuidToProposals;
 
   /// @param _tokenAddress Address of ERC20 token contract
   /// @param _quorumTokensPercentage Percentage of tokens needed to reach quorum
+  /// @param _proposerAmount Number of tokens required to submit a proposed tweet
   constructor(
     address _tokenAddress,
     uint256 _quorumTokensPercentage,
@@ -40,13 +43,13 @@ contract TwitterVoter { // CapWords
     proposerAmount = _proposerAmount;
   }
 
-  function propose(uint256 id) external returns (bool success) {
+  function propose(bytes32 id) external returns (bool success) {
     // Store
     if(!uuidToProposals[id]) {
       tokenAddress.transferFrom(msg.sender, this, proposerAmount);
 
       uuidToProposals[id] = Proposal({
-        proposer: msg.sender,
+        proposer: msg.sender, // Added for transparency, not internal usage
         startTime: now,
         open: true
       });
@@ -56,58 +59,81 @@ contract TwitterVoter { // CapWords
     return false;
   }
 
-  function vote(uint256 id, uint256 amount, bool vote) {
+  function vote(bytes32 id, uint256 amount, bool vote) external returns (bool success) {
     if(uuidToProposals[id] && uuidToProposal[id].open){
       tokenAddress.transferFrom(msg.sender, this, amount);
 
       if(vote){
-        uuidToProposals[id].yesVotes [msg.sender] = uuidToProposals[id].yesVotes [msg.sender] + amount;
+        uuidToProposals[id].yesVotes[msg.sender] = uuidToProposals[id].yesVotes[msg.sender] + amount;
         uuidToProposals[id].yesTotal = uuidToProposals[id].yesTotal + amount; 
       } else {
-        uuuidToProposals[id].noVotes [msg.sender] = uuuidToProposals[id].noVotes [msg.sender] + amount;
+        uuuidToProposals[id].noVotes[msg.sender] = uuuidToProposals[id].noVotes[msg.sender] + amount;
         uuidToProposals[id].noTotal = uuidToProposals[id].noTotal + amount; 
-      } 
+      }
+
+      return true;
     }
+
+    return false;
   }
 
-  function close(uint256 id) {
+  function close(bytes32 id) external returns (bool success) {
     if(
         uuidToProposals[id] && 
         uuidToProposal[id].open && 
-        uuidToProposal[id].startTime + 10 minutes > now
+        uuidToProposal[id].startTime + votingLength > now
       ){
         // 1. Close
         uuidToProposal[id].open = false;
 
         // 2. Determine winner
         uint256 minTokensRequired = quorumTokensPercentage.mul(tokenAddress.totalSupply).div(100);
-        if((uuidToProposals[id].noTotal + uuidToProposals[id].yesTotal) > minTokensRequired) { //quorum passed
-          if(uuidToProposals[id].yesTotal > uuidToProposals[id].noTotal) { //yes votes won
-            uuidToProposals[id].bonus = uuidToProposals[id].noTotal.mul(1000).div(uuidToProposals[id].yesTotal); //TODO fix 1000 
+
+        if((uuidToProposals[id].noTotal + uuidToProposals[id].yesTotal) > minTokensRequired) { // quorum passed
+          uuidToProposals[id].quorumPassed = true;
+          if(uuidToProposals[id].yesTotal > uuidToProposals[id].noTotal) { // yes votes won
+            uuidToProposals[id].bonus = uuidToProposals[id].noTotal.mul(1000).div(uuidToProposals[id].yesTotal); // TODO fix 1000 
             uuidToProposals[id].yesWon = true;
           } else { //no votes won
             uuidToProposals[id].bonus = uuidToProposals[id].yesTotal.div(uuidToProposals[id].noTotal);
             uuidToProposals[id].yesWon = false;
           }
         }
+        
+        return true;
       }
+    return false;
   }
 
-
-  function claim(uint256[] ids) {
+  function claim(bytes32[] ids) external (bool sent) {
     uint256 tokenSum; //TODO memory?? 
 
     for (i = 0; i < ids.length; i++){
-      if(uuidToProposals[ids[i].yesWon) {
+      if(!uuidToProposals[ids[i].quorumPassed) {
+        tokenSum = tokenSum + 
+          uuidToProposals[ids[i]].yesVotes[msg.sender] + 
+          uuidToProposals[ids[i]].noVotes[msg.sender];
+
+          uuidToProposals[ids[i]].yesVotes[msg.sender] = 0;
+          uuidToProposals[ids[i]].noVotes[msg.sender] = 0;
+      } else if(uuidToProposals[ids[i].yesWon) {
         tokenSum = tokenSum + 
                    uuidToProposals[ids[i]].yesVotes[msg.sender] + 
                    uuidToProposals[ids[i]].bonus.mul(uuidToProposals[ids[i]].yesVotes[msg.sender]).div(1000);
+
+        uuidToProposals[ids[i]].yesVotes[msg.sender] = 0;
       } else {
-      tokenSum = tokenSum + 
+        tokenSum = tokenSum + 
                    uuidToProposals[ids[i]].noVotes[msg.sender] + 
                    uuidToProposals[ids[i]].bonus.mul(uuidToProposals[ids[i]].noVotes[msg.sender]).div(1000);
+        uuidToProposals[ids[i]].noVotes[msg.sender] = 0;
       }
     }
-    tokenAddress.transferFrom(this, msg.sender, tokenSum);
 
+    if (tokenSum > 0) {
+      tokenAddress.transferFrom(this, msg.sender, tokenSum);
+      return true;
+    }
+
+    return false;
 }
