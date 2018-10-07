0c9e2674b74096bfadb8f8caef8c12175dfa106b
Nemil Dalal <nemild@gmail.com>
2018-10-06 21:15:21 -0700

    Fixed bug in mintable and approve contract

--- contracts/ERC20MintableAndApprove.sol
+++ contracts/ERC20MintableAndApprove.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.4.24;
 import "./ERC20Mintable.sol";
 
 contract ERC20MintableAndApprove is ERC20Mintable {
-  function mintAndApprove(address to, uint256 value) public onlyMinter returns(bool) {
+  function mintAndApprove(address to, address tweEthContractAddress, uint256 value) public onlyMinter returns(bool) {
     mint(to, value);
-    _allowed[to][msg.sender] = _allowed[to][msg.sender] + value;
+    _allowed[to][msg.sender] = _allowed[to][tweEthContractAddress] + value;
   }
 }
