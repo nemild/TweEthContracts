f586110469b7c93bcb800782a9c1d6f15545a290
Nemil Dalal <nemild@gmail.com>
2018-10-07 08:24:32 -0700

    Set quorum to 1

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,6 +5,6 @@ module.exports = function(deployer, network, accounts) {
   deployer.deploy(ERC20MintableAndApprove).then(function() {
     return ERC20MintableAndApprove.deployed();
   }).then(function(token) {
-    deployer.deploy(TweEthVoter, token.address, 5, {from: accounts[0]});
+    deployer.deploy(TweEthVoter, token.address, 1, {from: accounts[0]});
   });
 }
